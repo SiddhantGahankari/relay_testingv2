32b9364ddbe124ebd831736d83cd33d67f3fe272
Siddhant Gahankari <b24cm1058@iitj.ac.in>
2025-08-14 22:24:20 +0530

    Update relay_server.go

--- relay_server.go
+++ relay_server.go
@@ -1,50 +1,44 @@
-// relay_server.go
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"os"
 
 	libp2p "github.com/libp2p/go-libp2p"
-	"github.com/libp2p/go-libp2p/core/host"
 	circuitv2 "github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
 )
 
 func main() {
-	// ctx := context.Background()
 
-	// Create relay host listening on 0.0.0.0:PORT
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9001" // Render detected ws port
+	}
+	hostname := os.Getenv("RENDER_EXTERNAL_HOSTNAME")
+	if hostname == "" {
+		hostname = "localhost"
+	}
+
 	h, err := libp2p.New(
-		libp2p.ListenAddrStrings(
-			"/ip4/0.0.0.0/tcp/9000",
-			"/ip4/0.0.0.0/tcp/9001/ws",
-		),
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%s/ws", port)),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Start Circuit Relay v2 service
 	_, err = circuitv2.New(h)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Relay server started!")
+	publicAddr := fmt.Sprintf("/dns4/%s/tcp/%s/ws/p2p/%s", hostname, port, h.ID())
+
+	fmt.Println("✅ Relay server started!")
 	fmt.Println("PeerID:", h.ID())
-	for _, addr := range h.Addrs() {
-		fmt.Println("Listening on:", addr)
-	}
+	fmt.Println("Public Multiaddr (give this to clients):")
+	fmt.Println(publicAddr)
 
-	// Keep running
 	select {}
-}
-
-// Helper to print full multiaddrs
-func fullAddrs(h host.Host) []string {
-	var out []string
-	for _, addr := range h.Addrs() {
-		out = append(out, fmt.Sprintf("%s/p2p/%s", addr, h.ID()))
-	}
-	return out
 }
